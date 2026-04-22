8e04b046099a87dcbab47dc193bae02ee8473b85
Nestor-Sarmiento <nesdav1005@gmail.com>
2026-04-22 01:08:48 -0500

    Añadir seguridad a los ficheros restantes

--- ConexionDB.php
+++ ConexionDB.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start();
+if (!isset($_SESSION['usuario']) && !isset($_SESSION['clave'])) {
+    header("Location: index.php");
+    exit();
+}
 class ConexionDB
 {
     private $host = "localhost";
